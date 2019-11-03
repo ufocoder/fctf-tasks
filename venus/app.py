7eb58f647c67eb41d77204c55f50173d785b0120
Sergey Ufocoder <xufocoder@gmail.com>
2019-11-04 00:05:33 +0300

    Remove cors

--- venus/app.py
+++ venus/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS, cross_origin
 from morse import encode_text, decode_text
 
 app = Flask(__name__)
-CORS(app)
 
 text_error = 'enter help for all commands'
 text_author = 'there are not the droids you are looking for'
@@ -16,8 +14,6 @@ text_key = "{fctf3_mmmmmooorrssse}"
 def home():
     return render_template('index.html')
 
-
-@cross_origin()
 @app.route('/send', methods=['POST'])
 def send():
     try:
