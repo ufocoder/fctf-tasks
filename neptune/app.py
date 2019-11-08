8f663e2b4a243e290eb1157945e1baced1405d32
Sergey Ufocoder <xufocoder@gmail.com>
2019-11-08 17:15:41 +0300

    Add `Neptune`

---
+++ neptune/app.py
@@ -0,0 +1,21 @@
+import re
+
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+flag = "{fctf_valhalla}"
+pattern_ua = re.compile(r'(^|\b)neptune($|\b)', re.I)
+pattern_referrer = re.compile(r'(^|\b)boris($|\b)', re.I)
+
+@app.route('/')
+def home():
+    is_good_ua = re.match(pattern_ua, request.user_agent.string or '') is not None
+    is_good_referrer = re.match(pattern_referrer, request.referrer or '') is not None
+
+    if not is_good_ua:
+        return "Hey.. use special browser only!"
+    elif not is_good_referrer:
+        return "Wait.. http said you not from Boris? Go away!"
+    else:
+        return flag
